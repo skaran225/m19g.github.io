dadf7c4e3d6ee33ecfdbba13e016cd9c1f1a9234
Karan Singh <skaran225@gmail.com>
2017-12-09 03:05:19 +0530

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
+float multiply(float,float);
+float multiply_in_js(float,float); 
 int main()
 
 {
 
-   cout << "initialized";
+   float x=4,y=6;
+   cout << "initialized\n";
+   cout << "multiplying in js : "<<x<<" * "<<y<<":\n" <<multiply(x,y);
 
+}
+float multiply(float x, float y)
+
+{
+
+    return multiply_in_js(x, y);
+
+}
+
+float multiply_in_js(float x,float y){
+	
+	return x*y;
+	
 }
